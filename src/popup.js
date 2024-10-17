e1218b687c56da63c7620f436f8978d9cdbd6295
dod0lp <dodotv007@gmail.com>
2024-10-17 19:37:02 +0200

    Can set custom speed value.

--- src/popup.js
+++ src/popup.js
@@ -1,21 +1,17 @@
-const speedrateSet = 2.5;
+const speedrateDefault = 2.5;
 const qualitySet = "1080p";
 
-const speedBtn = document.getElementById("setSpeedBtn");
+const qualityBtn = document.getElementById("setQualityBtn");
 
-speedBtn.addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs.length > 0) {
-            const activeTab = tabs[0];
-            chrome.scripting.executeScript({
-                target: { tabId: activeTab.id },
-                func: runSpeedCommand,
-                args: [speedrateSet]
-            });
-        }
-    });
+// Ensure one that only one checkbox can be checked
+const checkboxContainer = document.getElementById('qualityCheckboxes');
+checkboxContainer.addEventListener('change', function(event) {
+    if (event.target.classList.contains('qualityOption')) {
+        const checkboxes = checkboxContainer.querySelectorAll('.qualityOption');
+        checkboxes.forEach(checkbox => {
+            if (checkbox !== event.target) {
+                checkbox.checked = false;
+            }
+        });
+    }
 });
-
-function runSpeedCommand(speedrate) {
-    document.getElementsByTagName("video")[0].playbackRate = speedrate;
-}
---
+++ src/setupSpeedBtn.js
@@ -0,0 +1,34 @@
+const speedBtn = document.getElementById("setSpeedBtn");
+
+function setSpeed(speedrate) {
+    document.getElementsByTagName("video")[0].playbackRate = speedrate;
+}
+
+function isValidFloat(value) {
+    const parsedValue = parseFloat(value);
+    
+    return !isNaN(parsedValue) && isFinite(parsedValue);
+}
+
+speedBtn.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs.length < 1) {
+            return;
+        }
+
+        const activeTab = tabs[0];
+
+        let speedValue = document.getElementById('speedInput').value;
+        if (!isValidFloat(speedValue)) {
+            speedValue = speedrateDefault;
+        }
+
+        chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            func: setSpeed,
+            args: [speedValue]
+        });
+
+        console.log(speedValue);
+    });
+});
