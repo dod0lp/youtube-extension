37558351691b13f7feaa89c0d0d2e7eac84c2d47
dod0lp <dodotv007@gmail.com>
2025-05-26 00:25:02 +0200

    Added mroe code with logging. Doesnt work for specific domain, need to find cookies saved on this tab somehow else.

--- src/clear-cookies/background.js
+++ src/clear-cookies/background.js
@@ -1,19 +1,37 @@
 chrome.action.onClicked.addListener(async (tab) => {
+    if (!tab.url.startsWith('http')) {
+        console.log('Invalid URL protocol:', tab.url);
+        return;
+    }
 
     const currentUrl = new URL(tab.url);
     const currentDomain = currentUrl.hostname;
-    const currentProtocol = currentUrl.protocol;
 
-    chrome.cookies.getAll({ domain: currentDomain }, (cookies) => {
-        for (const cookie of cookies) {
-            const cookieDomain = cookie.domain.startsWith('.') ?
-                cookie.domain.slice(1) : cookie.domain;
-            const cookieUrl = `${currentProtocol}//${cookieDomain}${cookie.path}`;
+    if (/(^|\.)google\./.test(currentDomain)) {
+        console.log('Blocked domain:', currentDomain);
+        return;
+    }
+    if (/(^|\.)youtube\./.test(currentDomain)) {
+        console.log('Blocked domain:', currentDomain);
+        return;
+    }
 
-            chrome.cookies.remove({
-                url: cookieUrl,
-                name: cookie.name
-            });
-        }
-    });
+  chrome.cookies.getAll({domain: currentDomain}, (cookies) => {
+    console.log(cookies.length);
+    
+    for (const cookie of cookies) {
+      let urlToClear = `${url.protocol}//${cookie.domain.replace(/^\./, '')}${cookie.path}`;
+      console.log(urlToClear);
+      console.log("fafd");
+
+      chrome.cookies.remove({
+        url: urlToClear,
+        name: cookie.name,
+        storeId: cookie.storeId
+      });
+    }
+
+    console.log("Reloading");
+    chrome.tabs.reload(tab.id);
+  });
 });
